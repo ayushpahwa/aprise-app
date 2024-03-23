3e972c33f2fb5299bfbe3be4f8c9ccd0fcd666cc
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-03-23 23:28:09 +0530

    create: validation configs for forms

---
+++ app/client/src/utils/formValidationConfigs.ts
@@ -0,0 +1,17 @@
+import { VALIDATION_EMAIL_INVALID, VALIDATION_EMAIL_REQUIRED, VALIDATION_PASSWORD_MIN_LENGTH, VALIDATION_PASSWORD_REQUIRED, createMessage } from './messages';
+
+export const emailValidationConfig = {
+  required: createMessage(VALIDATION_EMAIL_REQUIRED),
+  pattern: {
+    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+    message: createMessage(VALIDATION_EMAIL_INVALID),
+  },
+};
+
+export const passwordValidationConfig = {
+  required: createMessage(VALIDATION_PASSWORD_REQUIRED),
+  minLength: {
+    value: 6,
+    message: createMessage(VALIDATION_PASSWORD_MIN_LENGTH),
+  },
+};
