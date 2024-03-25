5ce638a67d13a293240fc21e185efe57c795572a
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-03-25 15:58:04 +0530

    update: login API changes

--- app/client/src/api/AuthAPI.ts
+++ app/client/src/api/AuthAPI.ts
@@ -6,17 +6,28 @@ export interface LoginDTO {
   password: string;
 }
 
-export interface LoginResponse {
+export interface RegisterDTO {
+  email: string;
+  password: string;
+  fullName: string;
+  defaultCurrencyId: number;
+}
+
+export interface AuthResponse {
   token: string;
 }
 
 class AuthAPI extends Api {
   static BASE = '/auth';
   static LOGIN = `${this.BASE}/login`;
-  static SIGNUP = `${this.BASE}/signup`;
+  static REGISTER = `${this.BASE}/register`;
 
-  static async login(loginData: LoginDTO): Promise<AxiosPromise<LoginResponse>> {
+  static async login(loginData: LoginDTO): Promise<AxiosPromise<AuthResponse>> {
     return Api.post(AuthAPI.LOGIN, loginData);
+  }
+
+  static async register(registerData: RegisterDTO): Promise<AxiosPromise<AuthResponse>> {
+    return Api.post(AuthAPI.REGISTER, registerData);
   }
 }
 
