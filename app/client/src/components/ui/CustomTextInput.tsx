02407af55f4194d66a6814ead2f4f0a1d5cb2da1
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-03-26 00:07:46 +0530

    update: added keyboard avoiding view and removed secure

--- app/client/src/components/ui/CustomTextInput.tsx
+++ app/client/src/components/ui/CustomTextInput.tsx
@@ -1,12 +1,15 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import { Colors } from '../../constants/styles';
 import { Control, Controller, FieldPath, RegisterOptions } from 'react-hook-form';
 import { VALIDATION_INVALID_INPUT, createMessage } from 'constants/messages';
+import { Input, ProgressBar } from '@ui-kitten/components';
 
 type FieldName = FieldPath<any>;
 
 interface Props {
+  autoCapitalize?: 'none' | 'sentences' | 'words' | 'characters';
+  autoComplete?: 'off' | 'email' | 'name';
   label: string;
   keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad';
   secure?: boolean;
@@ -15,32 +18,34 @@ interface Props {
   validationRules?: Omit<RegisterOptions<any, FieldName>, 'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'>; // Prop used by react-hook-form to define validation rules
 }
 
-function Input({ control, label, keyboardType, secure, name, validationRules = {} }: Props) {
+function CustomTextInput({ autoCapitalize = 'none', autoComplete, control, label, keyboardType, secure, name, validationRules = {} }: Props) {
   return (
     <Controller
       control={control}
       name={name}
       rules={validationRules}
       render={({ field: { value, onBlur, onChange }, fieldState: { error } }) => (
-        <View style={styles.inputContainer}>
+        <View style={[styles.inputContainer]}>
           <Text style={[styles.label, !!error && styles.labelInvalid]}>{label}</Text>
-          <TextInput
+          <Input
             style={[styles.input, !!error && styles.inputInvalid]}
-            autoCapitalize="none"
+            autoCapitalize={autoCapitalize}
+            autoComplete={autoComplete}
             keyboardType={keyboardType}
-            secureTextEntry={secure}
             onChangeText={onChange}
             onBlur={onBlur}
             value={value}
+            textStyle={!!secure ? { color: 'rgba(0,0,0,0)' } : {}} // Hide text in secure input
           />
-          {!!error && <Text style={{ color: Colors.error500 }}>{error.message || createMessage(VALIDATION_INVALID_INPUT)}</Text>}
+          {!!secure && <ProgressBar progress={(!!value ? value?.length : 0) / 6} status={(!!value ? value?.length : 0) < 3 ? 'danger' : 'success'} />}
+          {!!error && <Text style={styles.errorText}>{error.message || createMessage(VALIDATION_INVALID_INPUT)}</Text>}
         </View>
       )}
     />
   );
 }
 
-export default Input;
+export default CustomTextInput;
 
 const styles = StyleSheet.create({
   inputContainer: {
@@ -54,14 +59,15 @@ const styles = StyleSheet.create({
     borderColor: Colors.error500,
   },
   input: {
-    paddingVertical: 8,
-    paddingHorizontal: 6,
     borderColor: Colors.accent_gray,
     borderWidth: 1,
     borderRadius: 4,
     fontSize: 16,
   },
+  errorText: {
+    color: Colors.error500,
+  },
   inputInvalid: {
-    backgroundColor: Colors.error100,
+    borderColor: Colors.error100,
   },
 });
--- app/client/src/screens/Auth/index.tsx
+++ app/client/src/screens/Auth/index.tsx
@@ -1,7 +1,19 @@
 import AuthContent from 'components/Auth/AuthContent';
+import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 function AuthScreen() {
-  return <AuthContent />;
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="height" enabled>
+      <AuthContent />
+    </KeyboardAvoidingView>
+  );
 }
 
 export default AuthScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
