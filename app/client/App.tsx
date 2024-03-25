659402eb9658c5a33b0d8ac76cf469302f1a362f
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-03-25 15:57:27 +0530

    update: UI lib changes

--- app/client/App.tsx
+++ app/client/App.tsx
@@ -4,6 +4,8 @@ import { AuthStack } from 'screens/Root';
 
 import TransactionContextProvider from 'store/TransactionsContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ApplicationProvider } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
 const queryClient = new QueryClient();
 
@@ -11,9 +13,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
-        <TransactionContextProvider>
-          <AuthStack />
-        </TransactionContextProvider>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <TransactionContextProvider>
+            <AuthStack />
+          </TransactionContextProvider>
+        </ApplicationProvider>
       </QueryClientProvider>
     </NavigationContainer>
   );
