7d1d00faf7598f0eef5d9d5235066239f2d85863
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-04-04 15:13:01 +0530

    update: UI for groups screen

--- app/client/src/screens/Groups.tsx
+++ app/client/src/screens/Groups.tsx
@@ -1,29 +1,45 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import React, { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import GroupsAPI from 'api/GroupsAPI';
-import { ProgressBar } from '@ui-kitten/components';
 import { QUERY_KEYS } from 'api/ApiConstants';
+import SearchInput from 'components/ui/SearchInput';
+import { PLACEHOLDER_SEARCH_GROUPS, YOUR_GROUPS_TITLE, createMessage } from 'constants/messages';
+import CreateGroup from 'components/groups/CreateGroup';
+import { defaultStyles } from 'constants/styles';
+import GroupsList from 'components/groups/GroupsList';
 
 const Groups = () => {
+  const [searchText, setSearchText] = React.useState('');
   const { isLoading, data } = useQuery({
     queryKey: [QUERY_KEYS.FETCH_GROUPS],
     queryFn: GroupsAPI.getGroups,
     enabled: true,
   });
 
-  if (isLoading) {
-    return <ProgressBar />;
-  }
+  const groupsList = useMemo(() => {
+    if (isLoading) return [];
+    if (!!data) {
+      return data?.data;
+    }
+    return [];
+  }, [isLoading, data]);
 
   return (
-    <View>
-      <Text>Groups</Text>
-      {data?.data.map((group: any) => (
-        <Text key={group.id}>{group.name}</Text>
-      ))}
+    <View style={styles.container}>
+      <SearchInput value={searchText} onChangeText={setSearchText} placeholder={createMessage(PLACEHOLDER_SEARCH_GROUPS)} />
+      <CreateGroup />
+      <Text style={defaultStyles.titleText}>{createMessage(YOUR_GROUPS_TITLE)}</Text>
+      <GroupsList groupsList={groupsList} isLoading={isLoading} />
     </View>
   );
 };
 
 export default Groups;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    padding: 24,
+  },
+});
