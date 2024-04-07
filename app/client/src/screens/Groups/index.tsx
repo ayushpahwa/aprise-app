cfcab36df195c44606e00e6ff7a01ab1b25bebe2
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-04-07 21:56:45 +0530

    refactor: structure

--- app/client/src/screens/Groups/index.tsx
+++ app/client/src/screens/Groups/index.tsx
@@ -1,15 +1,18 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React, { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import GroupsAPI from 'api/GroupsAPI';
+import GroupsAPI, { Group, GroupType } from 'api/GroupsAPI';
 import { QUERY_KEYS } from 'api/ApiConstants';
 import SearchInput from 'components/ui/SearchInput';
-import { PLACEHOLDER_SEARCH_GROUPS, YOUR_GROUPS_TITLE, createMessage } from 'constants/messages';
-import CreateGroup from 'components/groups/CreateGroup';
+import { CREATE_GROUP_TITLE, PLACEHOLDER_SEARCH_GROUPS, YOUR_GROUPS_TITLE, createMessage } from 'constants/messages';
+import CreateGroupCard from 'components/groups/GroupTypeSelector';
 import { defaultStyles } from 'constants/styles';
 import GroupsList from 'components/groups/GroupsList';
+import { RootStackNavigationType, ScreenNamesEnum } from 'constants/navigationTypes';
+import { useNavigation } from '@react-navigation/native';
 
 const Groups = () => {
+  const navigation = useNavigation<RootStackNavigationType>();
   const [searchText, setSearchText] = React.useState('');
   const { isLoading, data } = useQuery({
     queryKey: [QUERY_KEYS.FETCH_GROUPS],
@@ -25,10 +28,15 @@ const Groups = () => {
     return [];
   }, [isLoading, data]);
 
+  const onSelectChange = (groupType: GroupType) => {
+    const params: Partial<Group> = { type: groupType };
+    navigation.navigate(ScreenNamesEnum.ROOT_MANAGE_GROUPS, params);
+  };
+
   return (
     <View style={styles.container}>
       <SearchInput value={searchText} onChangeText={setSearchText} placeholder={createMessage(PLACEHOLDER_SEARCH_GROUPS)} />
-      <CreateGroup />
+      <CreateGroupCard title={createMessage(CREATE_GROUP_TITLE)} onSelectChange={onSelectChange} />
       <Text style={defaultStyles.titleText}>{createMessage(YOUR_GROUPS_TITLE)}</Text>
       <GroupsList groupsList={groupsList} isLoading={isLoading} />
     </View>
