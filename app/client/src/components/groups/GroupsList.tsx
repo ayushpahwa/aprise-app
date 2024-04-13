2ccb270942610ac1db3468a16711ccd5bd06e215
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-04-13 18:30:50 +0700

    Update GroupsList.tsx

--- app/client/src/components/groups/GroupsList.tsx
+++ app/client/src/components/groups/GroupsList.tsx
@@ -1,8 +1,9 @@
-import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { Group } from 'api/GroupsAPI';
 import GroupListItem from './GroupListItem';
 import { ProgressBar } from '@ui-kitten/components';
+import { FlashList } from '@shopify/flash-list';
 
 interface Props {
   groupsList: Array<Group>;
@@ -13,7 +14,12 @@ const GroupsList = ({ groupsList, isLoading }: Props) => {
   return (
     <View style={styles.container}>
       {isLoading && <ProgressBar />}
-      <FlatList data={groupsList} renderItem={({ item }) => <GroupListItem group={item} />} keyExtractor={(item) => item.id.toString()} />
+      <FlashList
+        data={groupsList}
+        estimatedItemSize={10}
+        renderItem={({ item }) => <GroupListItem group={item} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
@@ -23,5 +29,6 @@ export default GroupsList;
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    height: '100%',
   },
 });
