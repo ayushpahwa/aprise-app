9d6dfcbb883d86f0eaf1aa1c3377bf757c0b809d
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-04-14 15:44:28 +0700

    Update TransactionListItem.tsx

--- app/client/src/components/TransactionListItem.tsx
+++ app/client/src/components/TransactionListItem.tsx
@@ -1,24 +1,32 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { TRANSACTION_TYPES } from 'constants/txnConstants';
-import { MaterialIcons as Icon } from '@expo/vector-icons';
 import moment from 'moment';
 import { Transaction } from 'constants/types/txnTypes';
+import { useMemo } from 'react';
 
 interface TransactionListItemProps {
   transaction: Transaction;
 }
 
 export const TransactionListItem = ({ transaction }: TransactionListItemProps) => {
-  const signMultiplier = transaction.type === TRANSACTION_TYPES.EXPENSE ? -1 : 1;
-  const formattedAmount = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(transaction.amount * signMultiplier);
+  const formattedAmount = useMemo(() => {
+    const signMultiplier = transaction.type === TRANSACTION_TYPES.EXPENSE ? -1 : 1;
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: transaction.currency.symbol,
+    }).format((transaction.amount / 100) * signMultiplier);
+  }, [transaction.amount, transaction.currency.symbol, transaction.type]);
+
   const pressHandler = () => {
     console.log('Transaction clicked', transaction.id);
   };
 
-  const txnDate = moment(transaction.createdAt).format('DD/MM/YYYY HH:mm');
+  // Format the time to show only hours and minutes and convert to local time from UTC
+  const txnDate = useMemo(() => {
+    const dateObj = moment(transaction.createdAt).add(moment().utcOffset(), 'minutes');
+    return dateObj.format('h:mm A');
+  }, [transaction.createdAt]);
+
   return (
     <Pressable style={styles.outerContainer} onPress={pressHandler}>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
@@ -31,14 +39,6 @@ export const TransactionListItem = ({ transaction }: TransactionListItemProps) =
             <Text>{txnDate}</Text>
           </View>
         </View>
-        <Icon
-          name="delete"
-          size={24}
-          color="red"
-          onPress={() => {
-            console.log('Delete transaction', transaction.id);
-          }}
-        />
       </View>
     </Pressable>
   );
@@ -54,6 +54,6 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'space-between',
     flexDirection: 'row',
-    width: '90%',
+    width: '100%',
   },
 });
