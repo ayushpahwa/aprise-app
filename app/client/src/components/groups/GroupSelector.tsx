37347e801f279d23980c8d474d0e11d92e06c2f0
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-04-14 15:44:46 +0700

    refactor: moved group data to modal

--- app/client/src/components/groups/GroupSelector.tsx
+++ app/client/src/components/groups/GroupSelector.tsx
@@ -3,41 +3,32 @@ import React from 'react';
 import { Control, Controller } from 'react-hook-form';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { IndexPath, Select, SelectItem } from '@ui-kitten/components';
-import { QUERY_KEYS } from 'api/ApiConstants';
-import GroupsAPI from 'api/GroupsAPI';
-import { useQuery } from '@tanstack/react-query';
+import { Group } from 'api/GroupsAPI';
 
 interface Props {
   control: Control<any>;
   name: string;
+  groups: Group[];
+  disabled?: boolean;
 }
 
-const GroupSelector = ({ control, name }: Props) => {
-  const {
-    data: groups,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: [QUERY_KEYS.FETCH_GROUPS],
-    queryFn: GroupsAPI.getGroups,
-  });
-
+const GroupSelector = ({ control, name, groups, disabled }: Props) => {
   return (
     <Controller
       control={control}
       name={name}
-      defaultValue={groups?.data?.length ? 0 : null}
+      defaultValue={groups?.length ? 0 : null}
       render={({ field: { onChange, value }, fieldState: { error } }) => (
         <View>
           <Text style={[styles.label, !!error && styles.labelInvalid]}>Group Selector</Text>
           <Select
-            disabled={isLoading || isError}
-            value={`${groups?.data[value]?.name || ''}`}
+            disabled={disabled}
+            value={`${groups[value]?.name || ''}`}
             onSelect={(index) => onChange((index as IndexPath).row)}
             placeholder="Select a group"
             style={{ width: '100%' }}
           >
-            {groups?.data.map((group) => (
+            {groups.map((group) => (
               <SelectItem key={group.id} title={group.name} />
             ))}
           </Select>
