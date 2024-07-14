c941e1610e163a47ba59db1ec1cc8fc9e70b60b3
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-07-14 21:53:50 +0700

    update: setup repo test for users

---
+++ app/server/src/test/java/in/aprise/finance/user/model/UsersRepositoryTest.java
@@ -0,0 +1,56 @@
+package in.aprise.finance.user.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UsersRepositoryTest {
+
+    @Autowired
+    private UsersRepository usersTest;
+
+    @AfterEach
+    void tearDown() {
+        usersTest.deleteAll();
+    }
+
+    @Test
+    void itShouldCheckIfUserExistsByEmail() {
+        // prepare
+        var email = "ayushpahwa96@gmail.com";
+        var user = User.builder().email(email).hashPassword("encodedTestPass")
+                .fullName("Ayush")
+                .createdAt(LocalDateTime.now()).isVerified(false).build();
+        usersTest.save(user);
+
+        // act
+        Optional<User> foundUser = usersTest.findByEmail(email);
+
+        // assert
+        assertEquals(foundUser.orElseThrow(),user);
+    }
+
+    @Test
+    void itShouldThrowErrorForNonExistentUser() {
+        // prepare
+        var email = "ayushpahwa96@gmail.com";
+        var wrongEmail = "ayush@appsmith.com";
+        var user = User.builder().email(email).hashPassword("encodedTestPass")
+                .fullName("Ayush")
+                .createdAt(LocalDateTime.now()).isVerified(false).build();
+        usersTest.save(user);
+
+        // act
+        Optional<User> foundUser = usersTest.findByEmail(wrongEmail);
+
+        // assert
+        assert(foundUser.isEmpty());
+    }
+}
