1f73ff516b9b2a583d4db4cb716af4863a9a6edf
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-08-11 00:08:17 +0530

    create: helper function for group req + res dtos

--- app/server/src/test/java/in/aprise/finance/group/GroupTestHelpers.java
+++ app/server/src/test/java/in/aprise/finance/group/GroupTestHelpers.java
@@ -1,9 +1,15 @@
 package in.aprise.finance.group;
 
+import in.aprise.finance.currency.Currency;
 import in.aprise.finance.group.model.Group;
 import in.aprise.finance.group.model.GroupMember;
 import in.aprise.finance.group.model.GroupType;
+import in.aprise.finance.group.model.dtos.CreateGroupRequestDTO;
+import in.aprise.finance.group.model.dtos.GroupResponseDTO;
 import in.aprise.finance.user.UserTestHelpers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupTestHelpers {
 
@@ -24,5 +30,23 @@ public class GroupTestHelpers {
                 .isOwner(true)
                 .isDefault(true)
                 .build();
+    }
+
+    public static CreateGroupRequestDTO createDefaultGroupRequestDTO(Long ownerId) {
+        List<Long> memberList = new ArrayList<>();
+        memberList.add(ownerId);
+        memberList.add(2L);
+
+        return CreateGroupRequestDTO.builder()
+                .name("Some group")
+                .description("My testing group")
+                .type(GroupType.PERSONAL)
+                .currencyId(1)
+                .members(memberList)
+                .build();
+    }
+
+    public static GroupResponseDTO createGroupResponseFromGroup(Group group, Currency currency) {
+        return GroupResponseDTO.builder().id(group.getId()).name(group.getName()).description(group.getDescription()).currencies(List.of(currency)).type(group.getType()).createdAt(group.getCreatedAt().toString()).build();
     }
 }
