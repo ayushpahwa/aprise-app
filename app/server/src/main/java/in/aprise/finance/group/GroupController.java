6e61c5bf8f55dfe12dc0f0745d0e2843420f9f52
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-08-11 01:27:08 +0530

    update: removed unused controller

--- app/server/src/main/java/in/aprise/finance/group/GroupController.java
+++ app/server/src/main/java/in/aprise/finance/group/GroupController.java
@@ -17,9 +17,10 @@ import org.springframework.web.bind.annotation.*;
 public class GroupController {
 
     private final GroupService groupService;
+
     @GetMapping("")
     public ResponseDTO<Object> getGroups() {
-        return new ResponseDTO<>(HttpStatus.OK.value(), groupService.getGroupsForCurrentUser(),"Groups fetched successfully");
+        return new ResponseDTO<>(HttpStatus.OK.value(), groupService.getGroupsForCurrentUser(), "Groups fetched successfully");
     }
 
     @PostMapping("")
@@ -31,21 +32,16 @@ public class GroupController {
         return ResponseEntity.status(status).body(formattedResponse);
     }
 
-    @GetMapping("/{groupId}")
-    public ResponseDTO<Object> getGroup(@PathVariable Long groupId) {
-        return new ResponseDTO<>(HttpStatus.OK.value(), groupService.getGroup(groupId),"Group fetched successfully");
-    }
-
     @GetMapping("/{groupId}/transactions")
     public ResponseDTO<Object> getAllTransactionsForGroup(@PathVariable Long groupId) {
-        return new ResponseDTO<>(HttpStatus.OK.value(), groupService.getGroupTransactions(groupId),"Transactions fetched successfully");
+        return new ResponseDTO<>(HttpStatus.OK.value(), groupService.getGroupTransactions(groupId), "Transactions fetched successfully");
     }
 
     @PostMapping("/{groupId}/transactions")
     public ResponseEntity<ResponseDTO<TransactionResponseDTO>> createTransaction(@Valid @RequestBody CreateTransactionDTO request, @PathVariable Long groupId) {
         String message = "Transaction created successfully";
         HttpStatus status = HttpStatus.CREATED;
-        TransactionResponseDTO response = groupService.createTransaction(request,groupId);
+        TransactionResponseDTO response = groupService.createTransaction(request, groupId);
         ResponseDTO<TransactionResponseDTO> formattedResponse = new ResponseDTO<>(status.value(), response, message);
         return ResponseEntity.status(status).body(formattedResponse);
     }
