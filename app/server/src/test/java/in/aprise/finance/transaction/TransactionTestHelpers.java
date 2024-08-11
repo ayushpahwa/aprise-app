6d48e84c7b3e05de825c248b50003a6dccc34049
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-08-11 15:48:04 +0530

    update: test helper for txns

--- app/server/src/test/java/in/aprise/finance/transaction/TransactionTestHelpers.java
+++ app/server/src/test/java/in/aprise/finance/transaction/TransactionTestHelpers.java
@@ -1,8 +1,20 @@
 package in.aprise.finance.transaction;
 
+import in.aprise.finance.transaction.model.Transaction;
+import in.aprise.finance.transaction.model.enums.TransactionSplitType;
+import in.aprise.finance.transaction.model.enums.TransactionType;
+
+import java.time.LocalDateTime;
+
 public class TransactionTestHelpers {
 
-    public static void createDefaultTxn() {
-        
+    public static Transaction createDefaultTxn() {
+        return Transaction.builder()
+                .amount(100)
+                .type(TransactionType.EXPENSE)
+                .splitType(TransactionSplitType.EQUAL)
+                .description("My transaction")
+                .created_at(LocalDateTime.now())
+                .build();
     }
 }
