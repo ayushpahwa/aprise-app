9e84007156b8f4a7a976690c70ca667162711837
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-08-11 15:48:27 +0530

    create: tests for txn repo func findByGroupId

--- app/server/src/test/java/in/aprise/finance/transaction/model/repository/TransactionRepositoryTest.java
+++ app/server/src/test/java/in/aprise/finance/transaction/model/repository/TransactionRepositoryTest.java
@@ -7,13 +7,21 @@ import in.aprise.finance.group.model.Group;
 import in.aprise.finance.group.model.GroupMember;
 import in.aprise.finance.group.model.repository.GroupMemberRepository;
 import in.aprise.finance.group.model.repository.GroupRepository;
+import in.aprise.finance.transaction.TransactionTestHelpers;
 import in.aprise.finance.transaction.model.Transaction;
 import in.aprise.finance.user.model.User;
 import in.aprise.finance.user.model.UsersRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 class TransactionRepositoryTest {
@@ -34,6 +42,7 @@ class TransactionRepositoryTest {
     User savedUser;
     GroupMember savedGroupMember;
     Transaction savedTransaction;
+    Currency savedCurrency;
 
     @BeforeEach
     void setUp() {
@@ -41,17 +50,87 @@ class TransactionRepositoryTest {
         var gm = GroupTestHelpers.createDefaultGroupMember();
         var userToSave = gm.getUser();
         userToSave.setDefaultCurrency(currency);
-        currencyRepository.save(currency);
+
+        savedCurrency = currencyRepository.save(currency);
         savedUser = usersRepository.save(userToSave);
         savedGroup = groupRepository.save(gm.getGroup());
         savedGroupMember = gmRepository.save(gm);
+
+        var transaction = TransactionTestHelpers.createDefaultTxn();
+        transaction.setCurrency(savedCurrency);
+        transaction.setCreatedByMember(savedGroupMember);
+        transaction.setGroup(savedGroup);
+        savedTransaction = repositoryUnderTest.save(transaction);
+    }
+
+    @AfterEach
+    void tearDown() {
+        repositoryUnderTest.deleteAll();
     }
 
     @Test
     void findByGroupId_itShouldReturnEmptyIfNoDataPresent() {
+        // prepare
+        var fakeGroupId = savedGroup.getId() + 1L;
+
+        // act
+        var response = repositoryUnderTest.findByGroupId(fakeGroupId);
+
+        // assert
+        assertTrue(response.isEmpty());
+    }
+
+    @Test
+    void findByGroupId_itShouldReturnEmptyIfTxnIsMarkedDeleted() {
+        // prepare
+        savedTransaction.set_deleted(true);
+        repositoryUnderTest.save(savedTransaction);
+
+        // act
+        var response = repositoryUnderTest.findByGroupId(savedGroup.getId());
+
+        // assert
+        assertTrue(response.isEmpty());
     }
 
     @Test
     void findByGroupId_itShouldTxnDataIfPresent() {
+        // prepare: let's create another transaction to be added
+        var transaction = TransactionTestHelpers.createDefaultTxn();
+        transaction.setCurrency(savedCurrency);
+        transaction.setCreatedByMember(savedGroupMember);
+        transaction.setGroup(savedGroup);
+        var savedTransaction2 = repositoryUnderTest.save(transaction);
+
+        List<Transaction> expectedResponse = new ArrayList<>();
+        expectedResponse.add(savedTransaction);
+        expectedResponse.add(savedTransaction2);
+
+        // act
+        var response = repositoryUnderTest.findByGroupId(savedGroup.getId());
+
+        // assert
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    void findByGroupId_itShouldReturnNotDeletedTxnDataOnly() {
+        // prepare: let's create another transaction to be added and set one as deleted
+        var transaction = TransactionTestHelpers.createDefaultTxn();
+        transaction.setCurrency(savedCurrency);
+        transaction.setCreatedByMember(savedGroupMember);
+        transaction.setGroup(savedGroup);
+        var savedTransaction2 = repositoryUnderTest.save(transaction);
+        savedTransaction.set_deleted(true);
+        repositoryUnderTest.save(savedTransaction);
+
+        List<Transaction> expectedResponse = new ArrayList<>();
+        expectedResponse.add(savedTransaction2);
+
+        // act
+        var response = repositoryUnderTest.findByGroupId(savedGroup.getId());
+
+        // assert
+        assertEquals(expectedResponse, response);
     }
 }
